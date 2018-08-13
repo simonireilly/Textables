e66dda34e91e0e9f3da7070959e1f17f8f5b419f
simonireilly <simonireilly@gmail.com>
2018-08-13 08:29:01 +0100

    Initial commit of sample application for react native using ignite

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
+import TextScreen from '../Containers/TextScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  TextScreen: { screen: TextScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'TextScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
